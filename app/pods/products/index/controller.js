ff359f250cda3882e25b99a1a8adaf9618dd153e
MBhambhani <milind.b@hotmail.com>
2020-11-13 19:35:55 -0500

    wrote templates for index and show

--- app/pods/products/index/controller.js
+++ app/pods/products/index/controller.js
@@ -2,6 +2,11 @@ import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
+  actions: {
+    click: function(button){
+      console.log(`The ${ button } button was clicked!`);
+    }
+  },
   publisher: alias('model.publisher'),
   products: alias('model.products')
 });
